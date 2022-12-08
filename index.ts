4e210636a696e64ac20043f53f876283ab26a9f6
sergey.ivashchenko <daranto@ukr.net>
2022-12-08 05:04:15 +0200

    new folder

--- index.ts
+++
@@ -1,9 +0,0 @@
-const puppeter = require('puppeteer');
-
-async function main() {
-	const browser = await puppeter.launch({ headless: false });
-	const page = await browser.newPage();
-	await page.goto('https://www.google.com');
-}
-
-main();
